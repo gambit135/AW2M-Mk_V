3fa94d01d037927bc61e6382f0bea86e1c6b8ccd
Alejandro Tellez G. <java.util.fck@hotmail.com>
2013-06-24 21:33:50 -0500

    3rd Commit of HolaMundo @AW2M-Mk_V

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -7,6 +7,6 @@
 public class HolaMundo {
 
     public static void Main(String args[]) {
-        System.out.println("Hola Mundo - Commit II");
+        System.out.println("Hola Mundo - Commit III");
     }
 }
