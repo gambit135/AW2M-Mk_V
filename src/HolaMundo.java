b3a38758929f5e5f152e5812881fea48796cbe65
Alejandro Tellez G. <java.util.fck@hotmail.com>
2013-06-24 21:33:12 -0500

    1st Commit of HolaMundo @AW2M-Mk_V

---
+++ src/HolaMundo.java
@@ -0,0 +1,12 @@
+
+/**
+ *
+ * @author Alejandro Téllez G. <java.util.fck@hotmail.com>
+ * @date 24/06/2013 - 09:31:05 PM
+ */
+public class HolaMundo {
+
+    public static void Main(String args[]) {
+        System.out.println("Hola Mundo - Commit I");
+    }
+}
